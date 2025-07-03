f46f2a762a4240c1f744ca69e7be2ef41bc0990e
Ivan Gotovchits <ivg@ieee.org>
2025-07-03 19:47:08 +0000

    ensures that only IPv{4,6} are passed to icmp protocol
    this prevents hangs on the assertions, so no more timeouts.

--- harnesses/icmp/harness.c
+++ harnesses/icmp/harness.c
@@ -7,6 +7,14 @@ int mayhem_process_input(char *buf, size_t len) {
         printf("the packet is too small\n");
         return 1;
     }
+    IPPacket_t *ip = ( IPPacket_t * )buf;
+
+    if (ip->xEthernetHeader.usFrameType != ipIPv4_FRAME_TYPE &&
+        ip->xEthernetHeader.usFrameType != ipIPv6_FRAME_TYPE) {
+        printf("discarding non-ip packet");
+        return 1;
+    }
+
     NetworkBufferDescriptor_t net = {};
     net.pucEthernetBuffer = buf;
     net.xDataLength = len;
