64f6e79c5dade700c0d44137ba92dfb86b83d639
Ivan Gotovchits <ivg@ieee.org>
2025-07-08 13:55:31 +0000

    adds neighborhood discovery protocol harness

---
+++ harnesses/nd-check-resolution/harness.c
@@ -0,0 +1,22 @@
+/* Include standard libraries */
+#include <stdio.h>
+#include "FreeRTOS_ND.h"
+
+int mayhem_process_input(char *buf, size_t len) {
+    if (len < sizeof(IPPacket_IPv6_t)) {
+        printf("the packet is too small\n");
+        return 1;
+    }
+    IPPacket_t *ip = ( IPPacket_t * )buf;
+
+    if (ip->xEthernetHeader.usFrameType != ipIPv6_FRAME_TYPE) {
+        printf("and ipv6 packet is required\n");
+        return 1;
+    }
+
+    NetworkBufferDescriptor_t net = {};
+    net.pucEthernetBuffer = buf;
+    net.xDataLength = len;
+    BaseType_t r = xCheckRequiresNDResolution(&net);
+    printf("processed ipv6 packet with the following outcome: %d\n", r);
+}
