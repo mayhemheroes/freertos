84aa1f135dee37fc2505e8728423044b726c8457
Ivan Gotovchits <ivg@ieee.org>
2025-08-28 18:45:19 +0000

    updates the main function to read all data

--- driver/main.c
+++ driver/main.c
@@ -8,8 +8,34 @@
 extern int mayhem_process_input(char *buf, size_t len);
 extern int mayhem_init(void);
 
-__attribute__((weak)) int mayhem_init(void) {
-    return 0;
+__attribute__((weak)) int mayhem_init(void) { return 0; }
+
+
+/** returns the contents of the file in a dynamically allocated buffer.
+
+    The file is read until we run out of memory or the descriptor returns EOF.
+    In case of any error, the function aborts the program.
+    We use malloc/realloc to ensure that asan knows the boundaries. */
+char *read_data(int fd, size_t *len) {
+    char *ptr = NULL;
+    *len = 0;
+    while (1) {
+        char buf[BUFSIZ];
+        ssize_t n = read(fd, buf, sizeof(buf));
+        if (n == -1) {
+            err(1, "read");
+        }
+        if (n == 0) {
+            break;
+        }
+        *len += n;
+        ptr = realloc(ptr, *len);
+        if (ptr == NULL) {
+            err(1, "malloc");
+        }
+        memcpy(ptr + *len - n, buf, n);
+    }
+    return ptr;
 }
 
 int main(int argc, char *argv[]) {
@@ -17,7 +43,6 @@ int main(int argc, char *argv[]) {
         errx(1, "aborted by mayhem_init");
     }
     int fd = 0;                 /* if no input read from the stdin */
-    char buf[BUFSIZ];
 
     if (argc > 1 && argv[1][0] != '-') {
         fd = open(argv[1], O_RDONLY);
@@ -25,21 +50,10 @@ int main(int argc, char *argv[]) {
             err(1, "open");
         }
     }
-
-    ssize_t len = read(fd, buf, sizeof(buf));
-    if (len == -1) {
-        err(1, "read");
-    }
+    size_t len = 0;
+    char *ptr = read_data(fd, &len);
     close(fd);                  /* it's okay to close stdin as well. */
-
-    /* we copy from the input buffer to make sure that address sanitizer
-     * knows perfectly where is the end of the buffer.  */
-    char *ptr = malloc(len);
-    if (ptr == NULL) {
-        err(1, "malloc");
-    }
-
-    int r = mayhem_process_input(memcpy(ptr, buf, len), len);
+    int r = mayhem_process_input(ptr, len);
     free(ptr);
     return r;
 }
